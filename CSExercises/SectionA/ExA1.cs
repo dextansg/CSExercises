57defcdee7d075665150d2f2c2782ebaaf1f153d
dextansg <tan.guoxiang@u.nus.edu>
2016-08-13 22:53:26 +0800

    Update ExA1.cs

--- CSExercises/SectionA/ExA1.cs
+++ CSExercises/SectionA/ExA1.cs
@@ -19,7 +19,8 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            //YOUR CODE HERE
+            Console.WriteLine("John Smith");
+            Console.WriteLine("e0011223 @u.nus.edu");
         }
     }
 }
